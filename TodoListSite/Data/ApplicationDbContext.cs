15a19b8c8bfe8463ccd93b3941dd2fe2c41d0d02
nieshuang <shuangneo@gmail.com>
2019-07-12 15:52:50 +0800

    添加sqllite数据库

--- TodoListSite/Data/ApplicationDbContext.cs
+++ TodoListSite/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TodoListSite.Models;
 
 namespace TodoListSite.Data
 {
@@ -12,5 +13,9 @@ namespace TodoListSite.Data
             : base(options)
         {
         }
+
+        public DbSet<TodoItem> Items { get; set; }
     }
+
+    
 }
