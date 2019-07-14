6dae8e419770ba4206a4916e7de6b5a9df022d18
nieshuang <shuangneo@gmail.com>
2019-07-14 13:01:04 +0800

    test fork push code without proxy

--- TodoListSite/Startup.cs
+++ TodoListSite/Startup.cs
@@ -62,7 +62,8 @@ namespace TodoListSite
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for 
+                // production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
