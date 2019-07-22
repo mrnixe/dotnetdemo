84e5b94fc0d0e5d41e4d81e28f366d2777fb51a3
nieshuang <shuangneo@gmail.com>
2019-07-22 21:27:42 +0800

    identityserver4

--- identutyserver4_neo/IdentityServer/Config.cs
+++ identutyserver4_neo/IdentityServer/Config.cs
@@ -19,12 +19,26 @@ namespace IdentityServer
 
         public static IEnumerable<ApiResource> GetApis()
         {
-            return new ApiResource[] { };
+            return new ApiResource[] {
+                new ApiResource("api1", "My API")
+             };
         }
 
         public static IEnumerable<Client> GetClients()
         {
-            return new Client[] { };
+            return new Client[] {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    } ,
+                    AllowedScopes = {"api1"}
+                }
+            };
+
         }
     }
 }
